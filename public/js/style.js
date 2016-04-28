771c658007521c4b944abeff07ba233ab8f4f4e3
Kirsten McAulay <mcaulay.kirsten@gmail.com>
2016-04-28 15:19:58 -0400

    updating buttons on home

--- public/js/style.js
+++ public/js/style.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 
-	$('#open').on('click', function(){
-		$('#shade').show();
-	});
-
+	$('.sign-in').on('click', function(){
+		$('.form').append('<div id="shade"></div>');
+		$('.form').append('')
 		$('#shade').on('click', function(){
-			$('#shade').hide();
+			$('#shade').remove();
+			})
+
+	})
+
+
+
+
+	// $('#open').on('click', function(){
+	// 	$('#shade').show();
+	// });
+
+	// 	$('#shade').on('click', function(){
+	// 		$('#shade').hide();
 		
-		})
+	// 	})
 	
 })
