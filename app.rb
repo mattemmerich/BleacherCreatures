aca447cad52515d7f615a3153d82190d2477697d
Kirsten McAulay <mcaulay.kirsten@gmail.com>
2016-04-29 16:12:12 -0400

    updating post function

--- app.rb
+++ app.rb
@@ -50,6 +50,7 @@ post '/sign-up' do
 	puts params.inspect
 	@user = User.new(fname: params[:fname], lname: params[:lname], username: params[:username], email: params[:email], password: params[:password])
 	@user.save
+	session[:user_id] = @user.id
 	redirect "/users/#{current_user.id}"
 end
 
